c08e343b9b3318d129fd8dd54d85634cdb19e99d
zephyr <linjie.zhang@outlook.com>
2023-06-07 17:02:11 +0800

    ,

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ interface Props {
 /* server component */
 export default function Header({}: Props) {
     return (
-        <header className='bg-[url("/images/bg.png")] lg:h-[350px] bg-cover h-[120px]'>
+        <header className='bg-[url("/images/bg.png")] lg:h-[350px] bg-cover h-[130px]'>
 
         </header>
     )
