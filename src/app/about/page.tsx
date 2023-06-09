7e6659ca4a9eb3bd41ff156e7614ce66eca3c375
zephyr <linjie.zhang@outlook.com>
2023-06-09 10:09:21 +0800

    .

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,23 +8,23 @@ interface Props {
 export default function AboutPage({}: Props) {
     return (
         <main className='bg-white'>
-            <section className='flex pl-[26px] pr-6 xl:pl-[70px] xl:pr-[95px] xl:pt-[67px] pt-[25px] xl:space-x-[62px] space-x-[23px]'>
+            <section className='flex flex-col items-center md:flex-row items-center md:items-start pl-[26px] pr-6 xl:pl-[70px] xl:pr-[95px] xl:pt-[67px] pt-[25px] xl:space-x-[62px] space-x-[23px]'>
                 <Image src='/images/about_us_person.png' alt='person' height={600} width={750}
-                    className='w-[180px] h-[230] xl:w-[495px] xl:h-[617px] max-w-[500px] max-h-[750px]'
+                    className='w-[80%] md:w-[50%]'
                 />
-                <div className='w-[230px] xl:w-[560px]'>
-                    <h3 className='text-center text-[8px] font-bold text-[var(--blue)] xl:text-xl pt-[6px] lg:pt-[16px]'>About us</h3>
+                <div className='w-[100%] md:w-[50%]'>
+                    <h3 className='text-center text-[8px] font-bold text-[var(--blue)] md:text-xl pt-[6px] lg:pt-[16px]'>About us</h3>
                     <div className='space-y-1 lg:space-y-2 relative'>
-                        <p className='text-[7px] lg:text-[14px] text-[#2F2F2F] leading-[1.5em] tracking-[0.01em] lg:leading-[30px] lg:tracking-[0.02em]'>
+                        <p className='text-[7px] md:text-[12px] lg:text-[13px] xl:text-[14px] text-[#2F2F2F] md:leading-[1.4em] tracking-[0.01em] lg:leading-[30px] lg:tracking-[0.02em]'>
                             Superunion is an international shipping agency, we are dedicate to providing secure, fast and low-cost international freight shipping service from China to the world, especially for door-to-door delivery for cross-border E-commerce business.
                         </p>
-                        <p className='text-[7px] lg:text-[14px] text-[#2F2F2F] leading-[1.5em] tracking-[0em] lg:leading-[30px] lg:tracking-[0.02em]'>
+                        <p className='text-[7px] md:text-[12px] xl:text-[14px] text-[#2F2F2F] md:leading-[1.4em] md:tracking-[0.01em] lg:leading-[30px] xl:tracking-[0.02em]'>
                             Our headquarters is located in Shenzhen, the center of the Pear River Delta, one of China’s most important regions for export and manufacturing, as well as the nearest and major port in China.
                         </p>
-                        <p className='text-[7px] lg:text-[14px] text-[#2F2F2F] leading-[1.5em] tracking-[0em] lg:leading-[30px] lg:tracking-[0.02em]'>
+                        <p className='text-[7px] md:text-[12px] xl:text-[14px] text-[#2F2F2F] md:leading-[1.4em] tracking-[0.01em] lg:leading-[30px] lg:tracking-[0.02em]'>
                             After years of exploring and developing, our team has accumulatd extensive experience and professional knowledges in providing suitable logistics solutions and dealing with various challenge. We believe that teamwork and collaboration are crucial for success, and that’s why we work closely with our clients to understand their needs and provide customized solutions that meet their specific requirements. We also prioritize customer satisfaction, and we strive to provide our clients with the best possible experience.
                         </p>
-                        <p className='text-[7px] lg:text-[14px] text-[#2F2F2F] leading-[1.5em] tracking-[0em] lg:leading-[30px] lg:tracking-[0.02em]'>
+                        <p className='text-[7px] md:text-[12px] xl:text-[14px] text-[#2F2F2F] md:leading-[1.4em] tracking-[0.01em] lg:leading-[30px] lg:tracking-[0.02em]'>
                             Our services include sea freight, air freight, express, inspection, FBA preparation, short-term storage, logistics consulting, customs brokerage. Whether you’re a large enterprise or a start-up, we have the skills and experience to help you achieve your goals.
                         </p>
                         <div className='absolute xl:-right-[18px] xl:top-[35px] xl:w-[7px] xl:h-[66px] bg-[#0052D9] top-[13px] -right-[7px] w-[3px] h-[33px]' />
